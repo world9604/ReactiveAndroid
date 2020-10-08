47c7730ab218fbdcffb645256ccea0cb971f6422
world9604 <world9604@gmail.com>
2020-10-08 16:23:46 +0900

    TAEIN - ReactiveAndroid2
    1. ReactiveAndroid2 프로젝트 추가

---
+++ ReactiveAndroid2/app/src/main/java/com/taein/reactiveandroid2/MainActivity.java
@@ -0,0 +1,47 @@
+package com.taein.reactiveandroid2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.taein.reactiveandroid2.databinding.ActivityMainBinding;
+
+import java.util.Date;
+
+import io.reactivex.Observable;
+import lombok.Getter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Getter
+    String helloText;
+    RecyclerView recyclerView;
+    private LinearLayoutManager layoutManager;
+    private StockDataAdapter stockDataAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setActivity(this);
+
+        binding.stockUpdatesRecyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        binding.stockUpdatesRecyclerView.setLayoutManager(layoutManager);
+
+        stockDataAdapter = new StockDataAdapter();
+        binding.stockUpdatesRecyclerView.setAdapter(stockDataAdapter);
+
+        Observable.just("Hello! Please use this app responsibly!")
+                .subscribe(s -> helloText = s);
+
+        Observable.just(
+                new StockUpdate("GOOGLE", 12.43, new Date()),
+                new StockUpdate("APPL", 645.1, new Date()),
+                new StockUpdate("TWTR", 1.43, new Date())).subscribe(stockDataAdapter::add);
+    }
+}
